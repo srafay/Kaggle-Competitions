bae35452900534b3f606a51673549a6edbb30b55
Syed Rafay <rafay_syed1@yahoo.com>
2017-09-11 15:47:07 +0500

    Adding 1.py, and updated all the info regarding it in Readme file

---
+++ 1.py
@@ -0,0 +1,48 @@
+# Data Preprocessing
+
+# Importing the libraries
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('train.csv')
+X = dataset.iloc[:, [2,4,5,6,7,9]].values
+y = dataset.iloc[:, 1].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
+imputer = imputer.fit(X[:, 2:3])
+X[:, 2:3] = imputer.transform(X[:, 2:3])
+imputer = imputer.fit(X[:, 5:6])
+X[:, 5:6] = imputer.transform(X[:, 5:6])
+
+
+from sklearn.preprocessing import LabelEncoder
+labelencoder_X = LabelEncoder()
+X[:, 1] = labelencoder_X.fit_transform(X[:, 1])
+
+
+# Fitting Naive Bayes to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X, y)
+
+
+testset = pd.read_csv('test.csv')
+Xtest = testset.iloc[:, [1,3,4,5,6,8]].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'most_frequent', axis = 0)
+imputer = imputer.fit(Xtest[:, 2:3])
+Xtest[:, 2:3] = imputer.transform(Xtest[:, 2:3])
+imputer = imputer.fit(Xtest[:, 5:6])
+Xtest[:, 5:6] = imputer.transform(Xtest[:, 5:6])
+
+labelencoder_Xtest = LabelEncoder()
+Xtest[:, 1] = labelencoder_Xtest.fit_transform(Xtest[:, 1])
+
+y_pred = classifier.predict(Xtest)
+
+import numpy
+numpy.savetxt("foo.csv", y_pred, delimiter=",", fmt='%.1d')
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# Kaggle-Titanic
-Applying Machine Learning to the Titanic's classification problem, from Kaggle
+# Titanic: Machine Learning from Disaster
+
+*Applying Machine Learning models to the classification problem - predicting the survival of the passengers on Titanic*
+
+### Submissions made to Kaggle
+
+1. **1.py**
+	* Using pclass, sex, age, Sibsp, parch, and fare features from the available dataset
+	* Using 'most frequent' strategy to replace the missing data
+	* Encoding the sex column (not using HotEncoders)
+	* Using Naive Bayes with default parameters
+	* Accuracy is 75.119%
+
